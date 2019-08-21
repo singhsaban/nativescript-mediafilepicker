f4c8d1ef02c9980d772237d019db912cdc6e9223
ssingh <ssingh@carlton.ca>
2019-08-21 17:22:58 -0400

    Removed ios functionality.

--- src/mediafilepicker.ios.d.ts
+++ src/mediafilepicker.ios.d.ts
@@ -1,29 +1,10 @@
 import { Observable } from 'tns-core-modules/data/observable';
 import { MediaPickerInterface, ImagePickerOptions, VideoPickerOptions, AudioPickerOptions, FilePickerOptions } from "./mediafilepicker.common";
 export declare class Mediafilepicker extends Observable implements MediaPickerInterface {
-    private _mediaPickerIQDeligate;
-    private _mediaPickerDocumentDeligate;
-    results: any;
-    msg: any;
     constructor();
     openImagePicker(params: ImagePickerOptions): void;
     openVideoPicker(params: VideoPickerOptions): void;
     openAudioPicker(params: AudioPickerOptions): void;
     openFilePicker(params: FilePickerOptions): void;
-    copyPHImageToAppDirectory(rawData: PHAsset, fileName: string): Promise<{}>;
-    copyPHVideoToAppDirectory(urlAsset: AVURLAsset, fileName: any): Promise<{}>;
-    convertPHImageToUIImage(rawData: PHAsset): Promise<{}>;
-    copyUIImageToAppDirectory(image: UIImage, fileName: any): Promise<{}>;
-    copyMPMediaFileToAPPDirectory(mediaItem: MPMediaItem, filename: any): Promise<{}>;
-    private presentViewController(controller);
     greet(): string;
-}
-export declare class MediafilepickerIQMediaPickerControllerDelegate extends NSObject implements IQMediaPickerControllerDelegate {
-    static ObjCProtocols: {
-        prototype: IQMediaPickerControllerDelegate;
-    }[];
-    private _owner;
-    static initWithOwner(owner: WeakRef<Mediafilepicker>): MediafilepickerIQMediaPickerControllerDelegate;
-    mediaPickerControllerDidFinishMedias(controller: IQMediaPickerController, selectedMedias: IQMediaPickerSelection): void;
-    mediaPickerControllerDidCancel(controller: IQMediaPickerController): void;
 }
